1b16ba4b50d411a392eae1032914d1e542256622
netogerbi <netogerbi2@hotmail.com>
2020-02-14 19:19:00 -0300

    correcting mongodb connection and nodejs version

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://localhost:27017/test',
+  mongoURI: 'mongodb://mongo:27017/test',
   cookieKey: '123123123'
 };
