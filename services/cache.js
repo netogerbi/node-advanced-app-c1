99632db49b3bbb4ad02d6219469e989dd5aca3ff
Reculos Gerbi Neto <reculos.neto@nscinfo.com.br>
2020-03-04 12:57:43 -0300

    getting mondoose data and showing in console

--- services/cache.js
+++ services/cache.js
@@ -5,5 +5,24 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = function() {
     console.log('Executando uma query');
 
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
+
     return exec.apply(this, arguments);
 }
+/*
+client.get = util.promisify(client.get);
+    const cachedBlogs = await client.get(req.user.id);
+    console.time('requisistion');    
+    if (cachedBlogs) {
+      console.timeEnd('requisistion');
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    console.timeEnd('requisistion');
+    console.log('SERVING FROM MONGO');
+    const blogs = await Blog.find({ _user: req.user.id });
+
+    res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));*/
